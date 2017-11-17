2d951525a90ac0193948f1eb2546cb24ef720c43
David Wagner <david.wagner@easymile.com>
2017-11-17 14:28:40 +0100

    Add options to choose an audio and a video source
    For now, we support dummy (test) sources for audio and video, v4l (eg.
    webcam) for video and pulseaudio for audio.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "Server.h"
+#include "sources.hpp"
 
 #include <boost/program_options.hpp>
 
@@ -9,6 +10,19 @@
 #include <chrono>
 
 namespace po = boost::program_options;
+
+template <class T>
+void validate(boost::any& v, const std::vector<std::string>& values, T*, int)
+{
+  po::validators::check_first_occurrence(v);
+  const std::string& s = po::validators::get_single_string(values);
+
+  try {
+    v = boost::any(T{T::presets.at(s)});
+  } catch (std::out_of_range&) {
+    throw po::validation_error(po::validation_error::invalid_option_value);
+  }
+}
 
 void usage(const po::options_description& desc)
 {
@@ -21,6 +35,7 @@ po::variables_map parseArgs(int argc, char** argv)
   po::options_description desc(
     "Spawns an RTSP Server publishing a single media; prints the URL in the standard output");
   desc.add_options()("help", "Show this help.");
+  desc.add_options()("presets", "List audio and video source presets.");
 
   desc.add_options()(
     "path", po::value<std::string>()->default_value("/example"), "Path to publish to.");
@@ -28,6 +43,15 @@ po::variables_map parseArgs(int argc, char** argv)
     "port",
     po::value<uint16_t>()->default_value(0),
     "Port to listen to. If 0, the port will be allocated dynamically.");
+  desc.add_options()(
+    "audio",
+    po::value<AudioSource>()->default_value({AudioSource::presets.at("dummy")}, "dummy"),
+    "Audio source preset.");
+  desc.add_options()(
+    "video",
+    po::value<VideoSource>()->default_value({VideoSource::presets.at("dummy")}, "dummy"),
+    "Video source preset.");
+
   po::variables_map vm;
   try {
     po::store(po::parse_command_line(argc, argv, desc), vm);
@@ -42,6 +66,12 @@ po::variables_map parseArgs(int argc, char** argv)
     exit(1);
   }
 
+  if (vm.count("presets")) {
+    printPresets<AudioSource>(std::cout, "Audio");
+    printPresets<VideoSource>(std::cout, "Video");
+    exit(1);
+  }
+
   return vm;
 }
 
@@ -50,7 +80,10 @@ int main(int argc, char** argv)
   auto vm = parseArgs(argc, argv);
   auto path = vm["path"].as<std::string>();
 
-  auto server = RtspServer{path, vm["port"].as<uint16_t>()};
+  auto server = RtspServer{path,
+                           vm["port"].as<uint16_t>(),
+                           vm["audio"].as<AudioSource>().value,
+                           vm["video"].as<VideoSource>().value};
   std::cout << "rtsp://127.0.0.1:" << server.port() << path << std::endl;
 
   while (true) {
